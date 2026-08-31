58ae9831f76cdd50336c6749e4016d4b36a7a5c5
Assad Anabosi <assad.anabosi@foothillsolutions.com>
2026-08-31 10:46:00 +0300

    feat:dockerize StatisticsCollector

--- StatisticsCollector/Models/ServerStatisticsConfig.cs
+++ StatisticsCollector/Models/ServerStatisticsConfig.cs
@@ -2,6 +2,8 @@
 
 public class ServerStatisticsConfig
 {
+    public const string SectionName = "ServerStatisticsConfig";
+    
     public int SamplingIntervalSeconds { get; set; }
     public string ServerIdentifier { get; set; } = string.Empty;
 }
--- StatisticsCollector/Program.cs
+++ StatisticsCollector/Program.cs
@@ -2,13 +2,15 @@
 using StatisticsCollector.Models;
 using StatisticsCollector.Utils;
 
+// TODO: DI Container
 var configuration = new ConfigurationBuilder()
     .SetBasePath(AppContext.BaseDirectory)
-    .AddJsonFile("appsettings.json")
+    .AddJsonFile("appsettings.json", optional: true)
+    .AddEnvironmentVariables() // Overrides appsettings.json
     .Build();
 
 var config = configuration
-    .GetSection("ServerStatisticsConfig")
+    .GetSection(ServerStatisticsConfig.SectionName)
     .Get<ServerStatisticsConfig>();
 
 var statisticsCollectorStrategy = StatisticsCollectorFactory.CreateCollector();
