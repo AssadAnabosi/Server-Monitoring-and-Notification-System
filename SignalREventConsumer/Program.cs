ce63cf360ccba0da0a2db727827924f372a521d7
Assad Anabosi <assad.anabosi@foothillsolutions.com>
2026-09-06 14:27:46 +0300

    feat:signalR event consumer

---
+++ SignalREventConsumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SignalRClientLibrary;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true)
+    .AddEnvironmentVariables()
+    .Build();
+
+var services = new ServiceCollection();
+services.AddOptions<SignalRHubOptions>()
+    .Bind(configuration.GetSection(SignalRHubOptions.SectionName));
+services.AddSingleton<SignalRAlertReceiver>();
+
+await using var provider = services.BuildServiceProvider();
+var receiver = provider.GetRequiredService<SignalRAlertReceiver>();
+receiver.AlertReceived += alert =>
+{
+    Console.WriteLine($"[{alert.Timestamp:O}] {alert.Type} - {alert.ServerIdentifier}");
+    Console.WriteLine(alert.Message);
+    Console.WriteLine($"Memory: {alert.MemoryUsagePercentage:P1}, CPU: {alert.CpuUsagePercentage:P1}");
+    return Task.CompletedTask;
+};
+
+await receiver.StartAsync();
+Console.WriteLine("Connected to the SignalR alert hub. Waiting for events...");
+Console.WriteLine("Press Ctrl+C to exit.");
+
+var completion = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+Console.CancelKeyPress += (_, eventArgs) =>
+{
+    eventArgs.Cancel = true;
+    completion.TrySetResult();
+};
+
+await completion.Task;
